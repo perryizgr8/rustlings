e73cb3c66b88bae6b5d5ab82804310c131af099f
Parikshit Misra <perryizgr8@live.in>
2021-02-21 18:31:37 +0530

    quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,11 +7,6 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
-// Put your function here!
-// fn ..... {
-
 // Don't modify this function!
 #[test]
 fn verify_test() {
@@ -20,4 +15,12 @@ fn verify_test() {
 
     assert_eq!(70, price1);
     assert_eq!(65, price2);
+}
+
+fn calculate_apple_price(num: i32) -> i32 {
+    if num > 40 {
+        num * 1
+    } else {
+        num * 2
+    }
 }
